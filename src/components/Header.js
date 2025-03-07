71887941befcb533210f35a387371cc304755900
Ralfy <kuba.maryska@seznam.cz>
2025-03-07 21:29:05 +0100

    signature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import SignatureAnimation from './SignatureAnimation';
 
 function Header() {
   return (
     <header className="header">
-{/*    <div className="logo">Jakub Maryška</div> */}
+      <div className="logo"><SignatureAnimation/></div>
       <nav className="nav">
         <a href="#about">About</a>
         <a href="#skills">Skills</a>
