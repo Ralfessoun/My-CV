52e3c352725bd018739bd5f6a43a447d778960ba
Ralfy <kuba.maryska@seznam.cz>
2025-03-10 21:34:51 +0100

    further optimalization for smaller screens

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './Contact.css';
+import SignatureAnimation from './SignatureAnimation';
 
 function Contact() {
   return (
     <section id="contact" className="contact">
-      <div className='contactbox'>
+      <div className="contact-container">
+        <div className='contactbox'>
+          
+          <h2>Contact</h2>
+          <p>Email: <a href="mailto:kuba.maryska@gmail.com">kuba.maryska@gmail.com</a></p>
+          <div className="contact-links">
+            <a href="https://github.com/Ralfessoun" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <span> | </span>
+            <a href="https://www.linkedin.com/in/ralfy/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          </div>
+        </div>
         
-      <h2>Contact</h2>
-      <p>Email: <a href="mailto:kuba.maryska@gmail.com">kuba.maryska@gmail.com</a></p>
-      <div className="contact-links">
-        <a href="https://github.com/Ralfessoun" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <span> | </span>
-        <a href="https://www.linkedin.com/in/ralfy/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
       </div>
-      </div>
+      <div className="signature-animation">
+          {/*  <SignatureAnimation />*/}
+        </div>
     </section>
   );
 }
